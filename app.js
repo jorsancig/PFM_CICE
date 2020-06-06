39b0de0fdb3d4094ebf126b661c66fb4c3378ea3
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-06 13:33:56 +0200

    Testeo rutas cliente - servidor_ - [+] Se ha añadido una ruta de prueba para verificar el proceso. Ha sido accesible tanto desde navegador (teto plano) como desde Postman.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const Express = require("express");
 const app = Express();
+const bodyParser = require("body-parser");
 const SERVER_PORT = process.env.SERVER_PORT || 5000;
+
+const index = require( "./routes" );
+
+app.use( "/raiz", index )
 
 
 app.listen(SERVER_PORT, () => {
