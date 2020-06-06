c13228ad74e783cbce9cc208c28e18b8d454fe49
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-06 21:19:57 +0200

    Depuración creación usuarios.

--- routes/user/create.js
+++ routes/user/create.js
@@ -4,10 +4,10 @@ const router = Express.Router();
 const User = require("../../models/User");
 
 router.post("/", async (req, res) => {
-  const { name, email, role, password, birth } = req.body;
+  const { name, email, role, password } = req.body;
 
   try {
-    const user = new User( { name, email, role, password, birth } );
+    const user = new User( { name, email, role, password } );
     console.log(user);
 
     const userDB = await user.save();
@@ -15,7 +15,7 @@ router.post("/", async (req, res) => {
     res.status(200).json({ userDB });
   } catch (error) {
     console.log(error);
-    res.status(400).json({ error });
+    res.status(400).json( { message: error.message, errors: error.errors, status: 400, ok: false } );
   }
 });
 
