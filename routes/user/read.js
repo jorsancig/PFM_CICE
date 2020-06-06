a4bff28549c19a07691d82ac34dd47d13d0bfc75
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-06 22:14:55 +0200

    Obtención datos mínimos usuario por ID.

---
+++ routes/user/read.js
@@ -0,0 +1,24 @@
+const Express = require("express");
+const router = Express.Router();
+const User = require("../../models/User");
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const options = {
+      new: true,
+      runValidators: true
+    };
+
+    const userDB = await User.find( { _id: id }, { name: 1, birth: 1, createdAt: 1, nickname: 1, _id : 0 });
+
+    res.json({ userDB });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+});
+
+
+module.exports = router;
