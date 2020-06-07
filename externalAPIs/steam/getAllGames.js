6077b3d7ed0c759ad27a508447e15eb26c18edbc
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-07 19:15:17 +0200

    try-catch getAllGames.js

--- externalAPIs/steam/getAllGames.js
+++ externalAPIs/steam/getAllGames.js
@@ -3,9 +3,16 @@ const router = Express.Router();
 const axios = require( 'axios' )
 
 router.get( '/', async( req, res ) => {
+    try {
+
+        const response = await axios.get( 'https://api.steampowered.com/ISteamApps/GetAppList/v1/' )
+        return res.status( 200 ).json( response.data.applist.apps )
     
-    const response = await axios.get( 'https://api.steampowered.com/ISteamApps/GetAppList/v1/' )
-    return res.status( 200 ).json( response.data.applist.apps )
+    } catch (error) {
+
+        return res.status( 500 ).json( { message: 'Something wrong happens.' } )
+
+    }
 } )
 
 
