6e83b01f64dac88fa52b0ad1aa07b4090f707a11
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-07 12:56:52 +0200

    Middlewares roles.

---
+++ middlewares/roles/isSuperAdminRole.js
@@ -0,0 +1,9 @@
+const rol = require( '../config/rol.json' )
+
+
+module.exports = (req, res, next) => {
+    if (req.session && req.session.currentUser) {
+      console.log(req.session.currentUser);
+      if (req.session.currentUser.role === rol.superAdmin) next();
+    } else res.status(401).json({ message: "Not superAdmin"})
+  };
