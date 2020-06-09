34757b1458e82bf44ba90d29e6e9e66da85e95e2
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-09 20:43:15 +0200

    Añadida ruta para editar juegos de colecciones.

--- routes/collections/index.js
+++ routes/collections/index.js
@@ -5,6 +5,7 @@ const router = Express.Router();
 
 router.use( '/add', require( './add' ) )
 router.use( '/delete', require( './delete' ) )
+router.use( '/edit', require( './update' ) )
 // router.use( '/login', require( './login' ) )
 // router.use( '/logout', require( './logout' ) )
 // router.use( '/useracces', require( './userAccess' ) )
