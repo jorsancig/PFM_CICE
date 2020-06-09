8c19d2b35050690ab26e690689c3d1f31c7fc680
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-09 21:03:46 +0200

    Obtención de toda una colección.

--- routes/collections/index.js
+++ routes/collections/index.js
@@ -6,6 +6,7 @@ const router = Express.Router();
 router.use( '/add', require( './add' ) )
 router.use( '/delete', require( './delete' ) )
 router.use( '/edit', require( './update' ) )
+router.use( '/collection', require( './getCollection' ) )
 // router.use( '/login', require( './login' ) )
 // router.use( '/logout', require( './logout' ) )
 // router.use( '/useracces', require( './userAccess' ) )
