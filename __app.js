808dc6a23d6d00782a2cdb18482bdc398ecbe1f1
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-28 18:55:36 +0200

    Testing heroku

---
+++ __app.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+const mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+
+const fileUpload = require("express-fileupload");
+
+var app = express();
+
+//-----------------------------------------------------------//
+// 1. CONFIGURACIÓN DE LA ESTRATEGIA DE PASSPORT LOCAL Y JWT //
+//-----------------------------------------------------------//
+
+require("./passport/config")(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//----------------------------//
+//    4. SUBIDA DE ARCHIVOS   //
+//----------------------------//
+// 4.1. configuración de middleware de recogida de archivos
+// app.use(fileUpload());
+// 4.2. (continúa en ./routes/upload/upload)
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).json({ message: "Not found" });
+});
+
+// mongoose
+//   .connect("mongodb://localhost:27017/auth-jwt", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log("Base de datos conectada"))
+//   .catch(() => {
+//     throw error;
+//   });
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var router = express.Router();
 
 // PAsamos el middleware como segundo parametro y desarrollamos la lógica de la función principal de la ruta index
-router.get("/", (req, res, next) => {
+router.get("/testconnection", (req, res, next) => {
   // en caso de entrar a la función, quiere decir que el usuario esta autorizado
-  res.json({ message: "Autorizado" });
+  res.json({ message: "CONNECTED" });
 });
 
 
