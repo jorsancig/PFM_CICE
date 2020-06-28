2dd5955be282b2163bf6f61b64c1018c0bdceecc
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-28 10:54:06 +0200

    Debug: búsqueda.

--- API/steam/search.js
+++ API/steam/search.js
@@ -5,43 +5,50 @@ const axios = require( 'axios' )
 router.get( '/:game', async( req, res ) => {
 
     const { game } = req.params;
-    
-    try {
+    if( game !== 'undefined' ){   
+        const regex = /[+]/g
+        const gameBlanks = game.replace( regex,' ' ).toLowerCase()
 
-        const response = await axios.get( 'https://api.steampowered.com/ISteamApps/GetAppList/v1/' )
-
-
-        // Searching by game name
-        const result = response.data.applist.apps.app.map( objectGame => {
-            if( objectGame.name.includes( game ) ){
-                return objectGame
-            }
-    
-        } )
+        console.log( 'Looking for: ', game, gameBlanks )
         
-        console.log( 'Getting serach response.' )
+        try {
 
-        const found = result.filter( element => { 
-            if( element != null ){
-                info = axios.get( `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${element.appid}&count=0&maxlength=0&format=json` )
-                element.image = `https://steamcdn-a.akamaihd.net/steam/apps/${element.appid}/header.jpg`
-                element.info = info
-                return element
-            }
+            const response = await axios.get( 'https://api.steampowered.com/ISteamApps/GetAppList/v2/' )
+
+            // Searching by game name
+            const result = response.data.applist.apps.map( objectGame => {
+                const gameName = objectGame.name.toLowerCase()
+                if( objectGame.name.toLowerCase().includes( gameBlanks ) ){
+                    return objectGame
+                }
+        
+            } )
             
-         } )
+            console.log( 'Getting search response.' )
+            // Return for no game
+            if( result.length <= 0 ) return res.status( 404 ).json( { message: 'Game not found' } )
+            const found = result.filter( element => { 
+                if( element != null ){
+                    // info = axios.get( `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${element.appid}&count=0&maxlength=0&format=json` )
+                    info = ''
+                    element.image = `https://steamcdn-a.akamaihd.net/steam/apps/${element.appid}/header.jpg`
+                    element.info = info
+                    return element
+                }
+                
+            } )
 
-        // Return for no game
-        if( found.length <= 0 ) return res.status( 404 ).json( { message: 'Game not found' } )
+            console.log( 'found' )
 
-        return res.status( 200 ).json( found )
+            return res.status( 200 ).json( { data: found } )
 
-    } catch (error) {
+        } catch (error) {
 
-        console.log( error )
-        return res.status( 500 ).json( { message: 'Something wrong happens!' } )
+            console.log( error )
+            return res.status( 500 ).json( { message: 'Something wrong happens!' } )
 
-        
+            
+        }
     }
 
 } )
