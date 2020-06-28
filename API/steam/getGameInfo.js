f0decd67986195a05d0327e64869a14419cd6eda
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-28 10:53:46 +0200

    Debug: getGameInfo.js

--- API/steam/getGameInfo.js
+++ API/steam/getGameInfo.js
@@ -5,15 +5,15 @@ const axios = require( 'axios' )
 router.get( '/:gameID', async( req, res ) => {
     
     const { gameID } = req.params;
-
+console.log( 'gameID', gameID )
     try {
-
-        const response = await axios.get( `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=400&count=3&maxlength=300&format=json` )
+        const response = await axios.get( `http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${gameID}&count=3&maxlength=300&format=json` )
         response.data.appnews.newsitems.steamURL = `https://store.steampowered.com/app/${gameID}`
-        return res.status( 200 ).json( response.data.appnews.newsitems )
+
+        return res.status( 200 ).json( response.data )
 
     } catch (error) {
-
+        // console.log( error )
         return res.status( 500 ).json( { message: 'Something wrong happens.' } )
 
     }
