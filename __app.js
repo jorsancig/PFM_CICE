c29086256c97075622fa2f9fe7a70261858b08ce
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-28 19:30:21 +0200

    Testing heroku

--- __app.js
+++ __app.js
@@ -15,7 +15,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 //----------------------------//
 //    4. SUBIDA DE ARCHIVOS   //
