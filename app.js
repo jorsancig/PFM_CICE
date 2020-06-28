824b0892eb72e5ec6d89219c0564363845719878
Jorge Sanjuán <jorsancig@gmail.com>
2020-06-28 17:33:55 +0200

    Testing heroku

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ const LocalStrategy = require( 'passport-local' ).Strategy
 
 
 // -- .env --
-const SERVER_PORT = process.env.SERVER_PORT || 5000;
-const SERVER_NAME =  process.env.SERVER || 'localhost';
+const SERVER_PORT = process.env.PORT || 5000;
+const SERVER_NAME =  process.env.HOST || '0.0.0.0';
 const DB_PORT = process.env.DB_PORT || '27027';
 const DB_NAME = process.env.DB_NAME || 'PFM_CICE';
 const SECRET = process.env.SECRET || 'basic-auth';
@@ -143,6 +143,8 @@ app.use( ( req, res ) => {
 
 
 
+
+// Leer localhost de variables y puertos
 
 
  
